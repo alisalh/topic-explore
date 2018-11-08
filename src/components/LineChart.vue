58c6e8be84ad1a34d433f781de9bcfaf0399d710
yunyou <1205361247@qq.com>
2018-11-08 15:10:46 +0800

    feat: add eslint support

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class=''>
-      hey
+  <div class="">
   </div>
 </template>
 
 <script>
 export default {
-  name: "component_name",
+  name: 'component_name',
   data () {
     return {
-    };
+    }
+  },
+  mounted () {
+    console.log('linechart mounted')
+    this.$axios.get('data/vue-all-versions-topic.csv', {
+    }).then(({ data }) => {
+      console.log(data)
+    })
   }
 }
 </script>
