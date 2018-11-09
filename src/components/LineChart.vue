c1c7b02e96855d0010db8d18012a3b20aacedd7f
yunyou <1205361247@qq.com>
2018-11-09 11:11:46 +0800

    feat: 1. add groupby utils 2. add dataAdapter in linechart component

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,20 +1,50 @@
 <template>
   <div class="">
+    hey
   </div>
 </template>
 
 <script>
+import * as d3 from 'd3'
 export default {
   name: 'component_name',
   data () {
     return {
+      topicsGroup: null
+    }
+  },
+  methods: {
+    groupBy (arr, prop) {
+      const propType = typeof (prop)
+      const cb = function (d) {
+        if (propType === 'string') return d[prop]
+        else return prop(d)
+      }
+      let propsCategory = [...new Set(arr.map(d => cb(d)))]; let res = []
+      propsCategory.forEach(p => {
+        res.push({
+          key: p,
+          val: arr.filter(d => cb(d) === p)
+        })
+      })
+      return res
+    },
+    dataAdapter (rawData) {
+      rawData.forEach(d => (d['Dominant_Topic'] = parseInt(d['Dominant_Topic'])))
+      let topicsGroup = this.groupBy(rawData, 'Dominant_Topic')
+      let verReg = /vue-\d*\.\d*\.\d*/
+      topicsGroup.forEach(d => {
+        d.val = this.groupBy(d.val, d => d.filename.match(verReg)[0])
+      })
+      return topicsGroup
     }
   },
   mounted () {
-    console.log('linechart mounted')
-    this.$axios.get('data/vue-all-versions-topic.csv', {
+    this.$axios.get('topics/getAllDocs', {
     }).then(({ data }) => {
-      console.log(data)
+      // const versions=data.map(d=>d.)
+      this.topicsGroup = this.dataAdapter(data)
+      console.log(this.topicsGroup)
     })
   }
 }
