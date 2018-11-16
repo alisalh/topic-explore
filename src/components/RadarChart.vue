e43f8c080bdb6ccb3a24815b35c7691945b71c4d
yunyou <1205361247@qq.com>
2018-11-16 10:50:54 +0800

    feat: add groupFileByStatus logic

--- src/components/RadarChart.vue
+++ src/components/RadarChart.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class='radar-chart'></div>
+  <div class='radar-chart'></div>
 </template>
 
 <script>
-import { getVersion } from '../utils'
+import { getVersion, getRelPath } from '../utils'
+import _ from 'lodash'
 export default {
   name: 'component_name',
   data () {
@@ -13,13 +14,38 @@ export default {
   },
   props: ['docVerData'],
   methods: {
-    groupFileByStatus (data) {}
+    /**
+     * 根据选中的版本获取将文件分为：修改的、新增的、删除的
+     */
+    groupFileByStatus (curVer) {
+      const docs = this.docVerData.files
+      const versions = this.docVerData.versions
+      const prevVer = this.getRelVersion(versions, curVer, -1)
+      const nextVer = this.getRelVersion(versions, curVer, 1)
+      const curDocs = docs.filter(d => getVersion(d.filename) === curVer)
+      const nextDocs = docs.filter(d => getVersion(d.filename) === nextVer)
+      const prevDocs = docs.filter(d => getVersion(d.filename) === prevVer)
+      const addDocs = _.differenceBy(curDocs, prevDocs, d =>
+        getRelPath(d['filename'])
+      )
+      const delDocs = _.differenceBy(prevDocs, curDocs, d =>
+        getRelPath(d['filename'])
+      )
+      const editDocs = _.intersectionBy(prevDocs, curDocs, d =>
+        getRelPath(d['filename'])
+      )
+      console.log(addDocs)
+      console.log(delDocs)
+      console.log(editDocs)
+    },
+    getRelVersion (versions, curVer, step) {
+      const idx = versions.indexOf(curVer)
+      return versions[idx + step]
+    }
   },
   created () {
     this.$bus.$on('version-selected', selectedVer => {
-      this.fileGroup = this.groupFileByStatus(
-        this.docVerData.filter(d => getVersion(d.filename) === selectedVer)
-      )
+      this.fileGroup = this.groupFileByStatus(selectedVer)
     })
   }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,7 @@ export function getVersion (fileName) {
   let verReg = /vue-(\d*\.\d*\.\d*)/
   return fileName.match(verReg)[1]
 }
+export function getRelPath (fileName) {
+  let verReg = /vue-(\d*\.\d*\.\d*)(.*)/
+  return fileName.match(verReg)[2]
+}
