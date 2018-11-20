1f10950ab004a2cc1a5604d71b947f6880520451
yunyou <1205361247@qq.com>
2018-11-20 19:29:37 +0800

    feat: add dominant topic in control panel

--- src/components/RadarControlPanel.vue
+++ src/components/RadarControlPanel.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class='radar-control-panel'>
-        <div class="topic-slider-control-panel">
-            <div v-for="item in sliderData"
-                 class='slider-control-wrapper'>
-                <div class="legend"
-                     :style="{background:item.color}">
-                </div>
-                <el-slider v-model="item.value"
-                           :min="0"
-                           :max="1"
-                           class='topic-slider'
-                           :step="0.1"></el-slider>
-            </div>
+  <div class='radar-control-panel'>
+    <div class="topic-slider-control-panel">
+      <div v-for="item in sliderData"
+           class='slider-control-wrapper'>
+        <div class="legend"
+             :style="{background:item.color}">
         </div>
-        <div class="dominant-topic-control-panel">
-
-        </div>
+        <el-slider v-model="item.value"
+                   :min="0"
+                   :max="1"
+                   class='topic-slider'
+                   :step="0.1"></el-slider>
+      </div>
     </div>
+    <hr class='divide-line' />
+    <div class="dominant-topic-control-panel">
+      <span class="title">Dominant Topic：</span>
+      <span>null</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -42,9 +44,6 @@ export default {
     .slider-control-wrapper {
       display: flex;
       align-items: center;
-      &:nth-child(n + 2) {
-        //   margin-top: 10px;
-      }
       .legend {
         width: 50px;
         height: 10px;
@@ -58,6 +57,9 @@ export default {
   }
   .dominant-topic-control-panel {
     flex: none;
+    .title {
+      font-weight: bold;
+    }
   }
 }
 </style>
