980212b36e1bee255dfb46e60f6eb2a928dba06f
alisalh <508091602@qq.com>
2020-05-15 10:45:36 +0800

    feat: 文件结构图修改节点图标

--- src/components/FileTree.vue
+++ src/components/FileTree.vue
@@ -8,6 +8,7 @@
 <script>
 import * as d3 from "d3";
 import d3tip from "d3-tip";
+import folderPng from "../assets/folder.png";
 
 export default {
   name: "component_name",
@@ -22,12 +23,13 @@ export default {
     };
   },
   computed: {},
-  props: ["topicColormap", "docData", "versions"],
+  props: ["topicColormap"],
   methods: {
     drawCurTree(data){
       d3.select('.cur-tree>*').remove();
+      let vm = this
 
-      var margin = {top: 15, right: 15, bottom: 30, left: 15},
+      var margin = {top: 20, right: 15, bottom: 20, left: 15},
         width = this.curWidth - margin.left - margin.right,
         height = this.curHeight - margin.top - margin.bottom;
       
@@ -63,11 +65,11 @@ export default {
           nodes = treeData.descendants(),
           links = treeData.descendants().slice(1);
 
-        nodes.forEach(d => d.y = d.depth * (height/maxDepth));
+        nodes.forEach(d => d.y = d.depth * (height/maxDepth) - 10);
 
         // node数据
         var node = svg.selectAll('g.node')
-          .data(nodes, function(d) { return d.id || (d.id = ++nodeId)});
+          .data(nodes, d => { return d.id || (d.id = ++nodeId)});
         
         // 为每一个新增的node添加g
         var nodeEnter = node.enter()
@@ -77,49 +79,40 @@ export default {
           .on('click', click);
         
         // 插入新增node
-        nodeEnter.append('circle')
-          .attr('r', 0)
-          .style('fill', function(d){
-            // if (d.data.type == 'file') return this.topicColormap(d.data.topic)
-            // if (d.data.type == 'dir') return 'gray'
-            // if (d.data.type == 'topic') return 'white'
-            return 'gray'
-          })
-          // .attr('stroke', function(d){
-          //   if (d.data.type == 'topic') return this.topicColormap(d.data.topicId)
-          //   else return null
-          // })
-        
-        // 插入节点文字
+        nodeEnter.append('text')
+          .attr('font-family', 'FontAwesome')
+          .attr('font-size', '20px')
+          .attr('dx', '-0.5em')
+          .attr('dy', '0.5em')
 
         // 更新进入的node
         var nodeUpdate = nodeEnter.merge(node);
         nodeUpdate.transition()
           .duration(750)
           .attr('transform', d => 'translate('+d.x+','+d.y+')');
-        nodeUpdate.select('circle')
-          .attr('r', function(d) {
-            if(d.data.type == 'file') return 5
-            else return 8
-            
+        nodeUpdate.select('text')
+          .attr('font-family', 'FontAwesome')
+          .attr('font-size', '20px')
+          .attr('dx', '-0.5em')
+          .attr('dy', '0.5em')
+          .style('fill', d => {
+            if(d.data.type == 'dir') return '#f19309'
+            if(d.data.type == 'file') return vm.topicColormap(d.data.topic)
+            if(d.data.type == 'topic') return vm.topicColormap(d.data.topicId)
           })
-          .style('fill', function(d){
-            // if (d.data.type == 'file') return this.topicColormap(d.data.topic)
-            // if (d.data.type == 'dir') return 'gray'
-            // if (d.data.type == 'topic') return 'white'
-            return 'gray'
+          .text(d => { 
+            if(d.data.type == 'dir') return "\uf07b" 
+            if(d.data.type == 'file') return "\uf1c9"
+            if(d.data.type == 'topic') return "\uf111"
           })
-          // .attr('stroke', function(d){
-          //   if (d.data.type == 'topic') return this.topicColormap(d.data.topicId)
-          //   else return null
-          // })
         
         // 删除退出的node
         var nodeExit = node.exit().transition()
           .duration(750)
           .attr('transform', 'translate('+source.x+','+source.y+')')
           .remove()
-        nodeExit.select('circle').attr('r', 0)
+        nodeExit.select('text') 
+          .attr('font-size', '0px')
 
         // link数据
         var link = svg.selectAll('path.link')
@@ -128,7 +121,7 @@ export default {
         // 为每一条新增的link添加g
         var linkEnter = link.enter().insert('path', 'g')
           .attr('class', 'link')
-          .attr('d', d => {
+          .attr('d', () => {
             let p = {x: source.x0, y: source.y0}
             return diagonal(p, p)    // link的平滑transition
           })
@@ -142,7 +135,7 @@ export default {
         // 删除退出的link
         var linkExit = link.exit().transition()
           .duration(750)
-          .attr('d', d => {
+          .attr('d', () => {
             let p = {x: source.x, y: source.y}
             return diagonal(p, p)
           })
@@ -176,8 +169,9 @@ export default {
     },
     drawPreTree(data){
       d3.select('.pre-tree>*').remove();
+      let vm = this
 
-      var margin = {top: 10, right: 15, bottom: 30, left: 15},
+      var margin = {top: 5, right: 15, bottom: 30, left: 15},
         width = this.preWidth - margin.left - margin.right,
         height = this.preHeight - margin.top - margin.bottom;
       
@@ -213,11 +207,11 @@ export default {
           nodes = treeData.descendants(),
           links = treeData.descendants().slice(1);
 
-        nodes.forEach(d => d.y = height - d.depth * (height/maxDepth));
+        nodes.forEach(d => d.y = height - d.depth * (height/maxDepth) + 10);
 
         // node数据
         var node = svg.selectAll('g.node')
-          .data(nodes, function(d) { return d.id || (d.id = ++nodeId)});
+          .data(nodes, d => { return d.id || (d.id = ++nodeId)});
         
         // 为每一个新增的node添加g
         var nodeEnter = node.enter()
@@ -227,49 +221,40 @@ export default {
           .on('click', click);
         
         // 插入新增node
-        nodeEnter.append('circle')
-          .attr('r', 0)
-          .style('fill', function(d){
-            // if (d.data.type == 'file') return this.topicColormap(d.data.topic)
-            // if (d.data.type == 'dir') return 'gray'
-            // if (d.data.type == 'topic') return 'white'
-            return 'gray'
-          })
-          // .attr('stroke', function(d){
-          //   if (d.data.type == 'topic') return this.topicColormap(d.data.topicId)
-          //   else return null
-          // })
-        
-        // 插入节点文字
+        nodeEnter.append('text')
+          .attr('font-family', 'FontAwesome')
+          .attr('font-size', '20px')
+          .attr('dx', '-0.3em')
+          .attr('dy', '0.2em')
 
         // 更新进入的node
         var nodeUpdate = nodeEnter.merge(node);
         nodeUpdate.transition()
           .duration(750)
           .attr('transform', d => 'translate('+d.x+','+d.y+')');
-        nodeUpdate.select('circle')
-          .attr('r', function(d) {
-            if(d.data.type == 'file') return 4
-            else return 8
-            
+        nodeUpdate.select('text')
+          .attr('font-family', 'FontAwesome')
+          .attr('font-size', '20px')
+          .attr('dx', '-0.3em')
+          .attr('dy', '0.2em')
+          .style('fill', d => {
+            if(d.data.type == 'dir') return '#f19309'
+            if(d.data.type == 'file') return vm.topicColormap(d.data.topic)
+            if(d.data.type == 'topic') return vm.topicColormap(d.data.topicId)
           })
-          .style('fill', function(d){
-            // if (d.data.type == 'file') return this.topicColormap(d.data.topic)
-            // if (d.data.type == 'dir') return 'gray'
-            // if (d.data.type == 'topic') return 'white'
-            return 'gray'
+          .text(d => { 
+            if(d.data.type == 'dir') return "\uf07b" 
+            if(d.data.type == 'file') return "\uf1c9"
+            if(d.data.type == 'topic') return "\uf111"
           })
-          // .attr('stroke', function(d){
-          //   if (d.data.type == 'topic') return this.topicColormap(d.data.topicId)
-          //   else return null
-          // })
         
         // 删除退出的node
         var nodeExit = node.exit().transition()
           .duration(750)
           .attr('transform', 'translate('+source.x+','+source.y+')')
           .remove()
-        nodeExit.select('circle').attr('r', 0)
+        nodeExit.select('text') 
+          .attr('font-size', '0px')
 
         // link数据
         var link = svg.selectAll('path.link')
@@ -351,7 +336,6 @@ export default {
     this.curWidth = Math.floor(this.$refs.root1.clientWidth);
     this.preHeight = Math.floor(this.$refs.root2.clientHeight);
     this.preWidth = Math.floor(this.$refs.root2.clientWidth);
-   
   }
 };
 </script>
